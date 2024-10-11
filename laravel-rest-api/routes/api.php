298d793cfe9439216aebcca629823d9d451bb4e5
Boda-M <mateboda05@gmail.com>
2024-10-11 13:16:20 +0200

    api/users works... sort of
    When unauthenticated it throws a massive internal server error instead 401, because f knows. I couldn't fix this with ChatGPT in an hour yesterday. People in the youtube comments also complained about this.

--- laravel-rest-api/routes/api.php
+++ laravel-rest-api/routes/api.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/users/login', [UsersController::class, 'login']);
 
-Route::get('/user', function (Request $request) {return $request->user(); })->middleware('auth:sanctum');
+Route::get('/users', function (Request $request) {return $request->user(); })->middleware('auth:sanctum');
