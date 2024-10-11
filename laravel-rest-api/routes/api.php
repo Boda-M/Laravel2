60decac2d7dfa47ddf04bf990f0406ba700e25ec
Boda-M <mateboda05@gmail.com>
2024-10-11 16:18:30 +0200

    api/products GET

--- laravel-rest-api/routes/api.php
+++ laravel-rest-api/routes/api.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
 Route::post('/users/login', [UsersController::class, 'login']);
-
+Route::get('/products', [ProductsController::class, 'index']);
 Route::get('/users', function (Request $request) {return $request->user(); })->middleware('auth:sanctum');
