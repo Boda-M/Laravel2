626958019c29ae3af2d8bc4982b5fe1cf2748e36
Boda-M <mateboda05@gmail.com>
2024-10-11 13:08:30 +0200

    Login endpoint working
    Took me 2+ hours becouse some auto generated file didn't update

---
+++ laravel-rest-api/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\UsersController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/users/login', [UsersController::class, 'login']);
+
+Route::get('/user', function (Request $request) {return $request->user(); })->middleware('auth:sanctum');
